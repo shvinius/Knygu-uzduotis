68b4d47008fd51d2be0af5a5cb0fbd9c6f299d9e
shvinius <shvinius@inbox.lt>
2022-06-09 09:34:03 +0300

    Knygu uzduotis

---
+++ KnyguKlase/Knyga.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnyguKlase
+{
+    internal class Knyga
+    {
+        private string isbn;
+        private string pavadinimas;
+        private string autorius;
+        private int leidimoMetai;
+        private string leidejas;
+        private string salis;
+        private decimal kaina;
+
+      
+        public Knyga(string isbn, string pavadinimas, string autorius, int leidimoMetai, string leidejas, string salis, decimal kaina)
+        {
+            this.isbn = isbn;
+            this.pavadinimas = pavadinimas;
+            this.autorius = autorius;
+            this.leidimoMetai = leidimoMetai;
+            this.leidejas = leidejas;
+            this.salis = salis;
+            this.kaina = kaina;
+        }
+
+        public Knyga()
+        {
+        }
+        public string Isbn { get => isbn; set => isbn = value; }
+        public string Pavadinimas { get => pavadinimas; set => pavadinimas = value; }
+        public string Autorius { get => autorius; set => autorius = value; }
+        public int LeidimoMetai { get => leidimoMetai; set => leidimoMetai = value; }
+        public string Leidejas { get => leidejas; set => leidejas = value; }
+        public string Salis { get => salis; set => salis = value; }
+        public decimal Kaina { get => kaina; set => kaina = value; }
+
+    }
+
+}
